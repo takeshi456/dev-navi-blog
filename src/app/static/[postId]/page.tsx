82bd6b59cd018728490be840493888563b903ad5
makloj <ysu.take7@gmail.com>
2023-09-01 15:44:01 +0900

    不要になったcssファイルを削除

--- src/app/static/[postId]/page.tsx
+++ src/app/static/[postId]/page.tsx
@@ -2,7 +2,6 @@ import { notFound } from "next/navigation";
 import parse from "html-react-parser";
 import { getDetail, getList } from "../../../../libs/microcms";
 import { css } from "../../../../styled-system/css";
-// import styles from "../../../../styles/Home.module.scss";
 
 export async function generateStaticParams() {
   const { contents } = await getList();
